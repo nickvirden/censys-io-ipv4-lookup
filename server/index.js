bdc2d958f2ea069d2e5752a8e86c933ecc9e0f0c
Nick Virden <nickvirden@gmail.com>
2023-03-01 00:16:03 -0600

    Attempt to get Docker working

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require("express");
+const path = require("path");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
+const HOST = '0.0.0.0';
 
 const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../client/public')));
 
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server listening on http://${HOST}:${PORT}`);
 });
