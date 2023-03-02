338bba91e6a50f0ee25343f30ba145c76c7c654f
Nick Virden <nickvirden@gmail.com>
2023-03-02 17:48:44 -0600

    Remove hello world text; change how loading is rendered using state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,18 @@
 import React, { useEffect, useState } from 'react';
 
-interface IResponseData {
-  message: string;
-}
-
 function App() {
-  const [data, setData] = useState<IResponseData | null>(null);
+  const [data, setData] = useState('');
 
   useEffect(() => {
     fetch('/api')
       .then((res) => res.json())
-      .then((data): Partial<IResponseData> => setData(data.message));
+      .then((data) => setData(data.message));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>hello world</div>
-        <p>{data !== {} || !data ? "Loading..." : data}</p>
+        <p>{!data ? "Loading..." : data}</p>
       </header>
     </div>
   );
