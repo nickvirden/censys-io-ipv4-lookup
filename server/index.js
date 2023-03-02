34f867d93d99320bc01ae88bf9b2b3c8c3a22784
Nick Virden <nickvirden@gmail.com>
2023-03-01 20:02:13 -0600

    Add GeoIP library to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const cors = require('cors');
-const express = require("express");
+const express = require('express');
+const { WebServiceClient } = require('@maxmind/geoip2-node');
 
 const PORT = 3001;
 const HOST = '0.0.0.0';
@@ -10,7 +11,13 @@ app.use(cors());
 
 app.options('*', cors());
 
+const client = new WebServiceClient('831191', 'DCuaaUACH19M2Wds', { host: 'geolite.info' });
+
 app.get('/api', (req, res) => {
+  client.country('142.1.1.1').then(response => {
+    console.log(response);
+  });
+
   res.json({ message: 'Hello from server!' });
 });
 
