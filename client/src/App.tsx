138f96e7e774db692b91e20e295f899cb0df15ce
Nick Virden <nickvirden@gmail.com>
2023-03-01 20:02:28 -0600

    Add React Bootstrap to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,23 @@
 import React, { useEffect, useState } from 'react';
 
+interface IResponseData {
+  message: string;
+}
+
 function App() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<IResponseData | null>(null);
 
   useEffect(() => {
     fetch('/api')
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data): Partial<IResponseData> => setData(data.message));
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <div>hello world</div>
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{data !== {} || !data ? "Loading..." : data}</p>
       </header>
     </div>
   );
