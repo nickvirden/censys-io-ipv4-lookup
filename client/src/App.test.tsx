9cf1878bb63be9aa39d67b0868049b915414ef19
Nick Virden <nickvirden@gmail.com>
2023-03-03 00:43:02 -0600

    Add some tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,8 +2,30 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // eslint-disable-next-line testing-library/no-node-access
+  const app = document.querySelector('#app');
+  expect(app).toBeInTheDocument();
+});
+
+describe('Submit button', () => {
+  it('has \'Submit\' text on page load', () => {
+    render(<App />);
+    
+    const buttonElement = screen.getByText('Submit');
+    
+    expect(buttonElement).toHaveTextContent('Submit');
+  });
+});
+
+describe('Longitude-Latitude display section', () => {
+  it('changes to a Spinner on Form submit', () => {
+    const { container } = render(<App />);
+
+    // eslint-disable-next-line testing-library/no-node-access, testing-library/no-container
+    const longLatContainer = container.querySelector('#long-lat-display');
+
+    expect(longLatContainer).toBeEmptyDOMElement();
+  });
 });
