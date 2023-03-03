2fec656d4a14b01bc37cad13d6e945e147680ce8
Nick Virden <nickvirden@gmail.com>
2023-03-02 20:04:34 -0600

    Get frontend and API call mostly sorted out

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { isEmpty } from 'lodash';
+import { Button, Container, Form, Spinner } from 'react-bootstrap';
+
+import './App.css';
 
 interface IResponseData {
   longitude?: number;
@@ -7,26 +10,72 @@ interface IResponseData {
 }
 
 function App() {
+  const [isFetching, setIsFetching] = useState(false);
   const [data, setData] = useState<IResponseData>({});
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+  async function fetchLongLat() {
+    if (inputRef?.current) {
+      setIsFetching(true);
+
+      await fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ ipAddress: inputRef.current.value })
+      })
+        .then((res) => res.json())
+        .then((data) => setData(data))
+        .catch((error) => console.log(error));
+
+      setIsFetching(false);
+    }
+  }
 
   return (
-    <div>
-      <div>
-        {
-          !isEmpty(data) ? (
-            <>
-              <div><b>Longitude:</b> { data?.longitude }</div>
-              <div><b>Latitude:</b> { data?.latitude }</div>
-            </>
-          ) : 'Loading...'
-        }
-      </div>
+    <div id="app">
+      <Container>
+        <div id="submission-form-box">
+          <div>
+            <Form onSubmit={(e) => e.preventDefault()} className="mb-3">
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>IPv4 Address</Form.Label>
+                <Form.Control type="text" placeholder="Enter IPv4 address" defaultValue="142.1.1.1" ref={inputRef} />
+                <Form.Text className="text-muted">
+                  Big brother is watching you. Just kidding! Or am I...?
+                </Form.Text>
+              </Form.Group>
+              <Button variant="primary" type="submit" disabled={isFetching} onClick={fetchLongLat}>
+                {
+                  isFetching ? (
+                    <>
+                      <Spinner
+                        as="span"
+                        animation="border"
+                        size="sm"
+                        role="status"
+                        aria-hidden="true"
+                      />
+                      <span className="visually-hidden">Loading...</span>
+                    </>
+                  ) : 'Submit'
+                }
+              </Button>
+            </Form>
+            <div id="long-lat-display">
+              {
+                !isEmpty(data) && (
+                  <>
+                    <div><b>Longitude:</b> { data?.longitude }</div>
+                    <div><b>Latitude:</b> { data?.latitude }</div>
+                  </>
+                )
+              }
+            </div>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -8,13 +8,15 @@ const HOST = '0.0.0.0';
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.options('*', cors());
 
 const client = new WebServiceClient('831191', 'DCuaaUACH19M2Wds', { host: 'geolite.info' });
 
-app.get('/api', async (req, res) => {
-  const response = await client.city('142.1.1.1');
+app.post('/api', async (req, res) => {
+  console.log("body:", req.body);
+  const response = await client.city(req.body.ipAddress);
   const { longitude, latitude } = response.location;
 
   res.json({ longitude, latitude });
